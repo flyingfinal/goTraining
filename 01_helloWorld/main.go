3feb35621b1b18323144b7cc5a37735502b1defc
flyingfinal <flyingfinal@gmail.com>
2018-06-24 13:06:01 -0700

    This tests the github upload

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(f1())
 }
 
 // This is comment
+
+func f1() (int, float64){
+	var x float64 = 1.6
+	y := x
+	return int(x),  y
+}
