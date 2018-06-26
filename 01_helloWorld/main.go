b33caaa2642b66be18696a58666be48378fc9d78
flyingfinal <flyingfinal@gmail.com>
2018-06-25 20:36:53 -0700

    This tests the github upload

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -3,13 +3,42 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(f1())
+
+	x := make([]int, 5, 10)
+	x[2] = 0
+
+	defer f1(x...)
+
+	Oddfunc := getOdd()
+	i := 0
+	for i < 10 {
+		fmt.Println(Oddfunc())
+		i++
+	}
 }
 
 // This is comment
 
-func f1() (int, float64){
-	var x float64 = 1.6
-	y := x
-	return int(x),  y
+func f1(arr ...int) (int, int){
+	x := 0
+	y := 0
+	for i,v := range arr {
+		y += v
+		fmt.Println(v, " ", i)
+		x += i
+	}
+
+
+	return x,  y
+}
+
+
+func getOdd() func() (int){
+	i := 1
+	return func() (int){
+		ret := i
+		i += 2
+		return ret
+	}
+
 }
